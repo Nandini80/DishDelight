514c3a14dacc8207c7fe3d69881a9891481aafea
Nandini Jindal <nandinijindal010@gmail.com>
2024-08-31 15:46:02 +0530

    footer

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
   return (
     <footer className="bg-dark text-white py-3 mt-5">
       <Container className="text-center">
-        <p className="mb-0">Your Source for Tasty Food Narratives</p>
-        <small className="text-muted">Proudly powered by WordPress | Theme: Listorify by CozyThemes.</small>
+        <p className="mb-0">We care about your love for food</p>
+        <small className="text-white">Copywrite @Nandini Jindal</small>
       </Container>
     </footer>
   );
